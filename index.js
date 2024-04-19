9e89ef82a1ed7cbf06a47e38e409fe72612adadd
bast1ann <163662587+bast1ann@users.noreply.github.com>
2024-04-19 16:44:21 -0400

    Simplify squares array

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 const container = document.querySelector(".container");
-const rows = [];
-const squares = [];
 const resetButton = document.querySelector(".resetButton");
+const squares = [];
 let nRows = 16;
-
-createGrid(nRows);
 
 resetButton.addEventListener("click", resetGrid);
 resetButton.addEventListener("mouseover", () => resetButton.style.backgroundColor = "#F0AB6C");
 resetButton.addEventListener("mouseout", () => resetButton.style.backgroundColor = "#74EDDA");
 
-// Creating rows
-function createGrid(dimension) {
-  for (let i = 0; i < dimension; i++) {
-    rows[i] = document.createElement("div");
-    rows[i].style.display = "flex";
-    container.appendChild(rows[i]);
-    squares[i] = [];
-    for (let j = 0; j < dimension; j++) {
-    squares[i][j] = document.createElement("div");
-    squares[i][j].style.border = "1px solid black";
-    squares[i][j].style.width = "80px";
-    squares[i][j].style.height = "80px";
-    squares[i][j].style.margin = "1px";
-    squares[i][j].addEventListener("mouseover", () => squares[i][j].style.backgroundColor = "red");
-    rows[i].appendChild(squares[i][j]);
-    }
+createSquares(nRows);
+
+// Create squares
+function createSquares(dimensions) {
+  const squareSize = 800/dimensions; 
+  for (let i = 0; i < dimensions*dimensions; i++) {
+    squares[i] = document.createElement("div");
+    squares[i].style.width = squareSize + "px";
+    squares[i].style.height = squareSize + "px";
+    squares[i].style.border = "1px solid grey";
+    squares[i].addEventListener("mousemove", () => squares[i].style.backgroundColor = "red");
+    container.appendChild(squares[i]);
   }
 }
 
 // Reset grid
 function resetGrid() {
-  dimensions = prompt("Enter dimensions of the grid:");
-  rows.forEach( row => row.remove());
-  createGrid(dimensions);
+  nRows = prompt("Enter dimensions of the grid:");
+  squares.forEach( square => square.remove());
+  createSquares(nRows);
 }
+
+// move in squares matrix
+
+// square.addEventListener("mousedown", () => pintar + activar mousemove event)
+// square.addEventListener("mouseup", () => desactivar mousemove event)
+
 
 // testing branch
