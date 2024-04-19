8a412ea2341d904057be3dc0ed6ff1a4f86481df
bast1ann <163662587+bast1ann@users.noreply.github.com>
2024-04-19 14:01:26 -0400

    Revert "Change grid to a fixed size"
    This reverts commit 0117eab44f945a78c6f8789c823a840cafd756e2.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
-const resetButton = document.querySelector(".resetButton");
 const rows = [];
 const squares = [];
+const resetButton = document.querySelector(".resetButton");
 let nRows = 16;
 
 createGrid(nRows);
@@ -12,7 +12,6 @@ resetButton.addEventListener("mouseout", () => resetButton.style.backgroundColor
 
 // Creating rows
 function createGrid(dimension) {
-  const squareSize = 800/dimension; 
   for (let i = 0; i < dimension; i++) {
     rows[i] = document.createElement("div");
     rows[i].style.display = "flex";
@@ -20,8 +19,10 @@ function createGrid(dimension) {
     squares[i] = [];
     for (let j = 0; j < dimension; j++) {
     squares[i][j] = document.createElement("div");
-    squares[i][j].style.width = squareSize.toString() + "px";
-    squares[i][j].style.height = squareSize.toString() + "px";
+    squares[i][j].style.border = "1px solid black";
+    squares[i][j].style.width = "80px";
+    squares[i][j].style.height = "80px";
+    squares[i][j].style.margin = "1px";
     squares[i][j].addEventListener("mouseover", () => squares[i][j].style.backgroundColor = "red");
     rows[i].appendChild(squares[i][j]);
     }
@@ -31,7 +32,7 @@ function createGrid(dimension) {
 // Reset grid
 function resetGrid() {
   dimensions = prompt("Enter dimensions of the grid:");
-  rows.forEach(row => row.remove());
+  rows.forEach( row => row.remove());
   createGrid(dimensions);
 }
 
