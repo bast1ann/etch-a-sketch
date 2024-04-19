12e25a879d825c71f118c2e2d822cfb89f9f1369
bast1ann <163662587+bast1ann@users.noreply.github.com>
2024-04-18 22:37:50 -0400

    Add functionality to reset button
    When clicking reset button you can enter the number of rows you want your grid to be.

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const container = document.querySelector(".container");
-const row = [];
-const square = [];
+const rows = [];
+const squares = [];
 const resetButton = document.querySelector(".resetButton");
-let dimensions = 16;
+let nRows = 16;
+
+createGrid(nRows);
+
+resetButton.addEventListener("click", resetGrid);
+resetButton.addEventListener("mouseover", () => resetButton.style.backgroundColor = "#F0AB6C");
+resetButton.addEventListener("mouseout", () => resetButton.style.backgroundColor = "#74EDDA");
 
 // Creating rows
-for (let i = 0; i < dimensions; i++) {
-  row[i] = document.createElement("div");
-  row[i].style.display = "flex";
-  container.appendChild(row[i]);
-  square[i] = [];
-  // Creating squares
-  for (let j = 0; j < dimensions; j++) {
-    square[i][j] = document.createElement("div");
-    square[i][j].style.border = "1px solid black";
-    square[i][j].style.width = "80px";
-    square[i][j].style.height = "80px";
-    square[i][j].style.margin = "1px";
-    square[i][j].addEventListener("mouseover", () => square[i][j].style.backgroundColor = "red");
-    row[i].appendChild(square[i][j]);
+function createGrid(dimension) {
+  for (let i = 0; i < dimension; i++) {
+    rows[i] = document.createElement("div");
+    rows[i].style.display = "flex";
+    container.appendChild(rows[i]);
+    squares[i] = [];
+    for (let j = 0; j < dimension; j++) {
+    squares[i][j] = document.createElement("div");
+    squares[i][j].style.border = "1px solid black";
+    squares[i][j].style.width = "80px";
+    squares[i][j].style.height = "80px";
+    squares[i][j].style.margin = "1px";
+    squares[i][j].addEventListener("mouseover", () => squares[i][j].style.backgroundColor = "red");
+    rows[i].appendChild(squares[i][j]);
+    }
   }
 }
 
-// Reset button
-resetButton.addEventListener("click", () => dimensions = prompt("Enter dimensions of the grid:"));
-resetButton.addEventListener("mouseover", () => resetButton.style.backgroundColor = "#F0AB6C");
-resetButton.addEventListener("mouseout", () => resetButton.style.backgroundColor = "#74EDDA");
+// Reset grid
+function resetGrid() {
+  dimensions = prompt("Enter dimensions of the grid:");
+  rows.forEach( row => row.remove());
+  createGrid(dimensions);
+}
