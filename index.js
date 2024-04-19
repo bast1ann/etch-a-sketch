ab5aeb7bde77cc11aaacc8b7cd1a97a8bf72d761
bast1ann <163662587+bast1ann@users.noreply.github.com>
2024-04-18 22:05:04 -0400

    Add hover effect over grid and a button to input dimensions of the grid

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const container = document.querySelector(".container");
 const row = [];
 const square = [];
+const resetButton = document.querySelector(".resetButton");
+let dimensions = 16;
 
 // Creating rows
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < dimensions; i++) {
   row[i] = document.createElement("div");
   row[i].style.display = "flex";
   container.appendChild(row[i]);
+  square[i] = [];
   // Creating squares
-  for (let j = 0; j < 16; j++) {
-    square[j] = document.createElement("div");
-    square[j].style.border = "2px solid black";
-    square[j].style.width = "80px";
-    square[j].style.height = "80px";
-    square[j].style.margin = "5px";
-    row[i].appendChild(square[j]);
+  for (let j = 0; j < dimensions; j++) {
+    square[i][j] = document.createElement("div");
+    square[i][j].style.border = "1px solid black";
+    square[i][j].style.width = "80px";
+    square[i][j].style.height = "80px";
+    square[i][j].style.margin = "1px";
+    square[i][j].addEventListener("mouseover", () => square[i][j].style.backgroundColor = "red");
+    row[i].appendChild(square[i][j]);
   }
 }
 
+// Reset button
+resetButton.addEventListener("click", () => dimensions = prompt("Enter dimensions of the grid:"));
+resetButton.addEventListener("mouseover", () => resetButton.style.backgroundColor = "#F0AB6C");
+resetButton.addEventListener("mouseout", () => resetButton.style.backgroundColor = "#74EDDA");
